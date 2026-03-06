9fec7183940720c13a1f483672bf77dfec923163
MiguelB5119 <miguel.beira387@education.nsw.gov.au>
2026-03-06 16:26:31 +1100

    character_customisation

--- gamified_studying.py
+++ gamified_studying.py
@@ -2,14 +2,28 @@ import os
 
 def main():
     character_customisation()
+    main_menu()
 
 def character_customisation():
-    filepath = "player_stat"
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(script_dir, "player_stats.txt")
     try:
-        with open(filepath, "x") as file:
-            file.write("It exists.")
+        with open(filepath, "x"):
+            print("No save file found. Creating one now...")
     except FileExistsError:
-        print("The file 'player_stats' already exists.")
-        print("Saved file to:", os.getcwd())
+        with open(filepath, "r") as file:
+            print(file.read())
+    else:
+        with open(filepath, "w") as file:
+            while True:
+                name = input("Please input your name. It must be between 2 and 16 characters: ")
+                if 2 <= len(name) <= 16:
+                    file.write(f"Name: {name}\n")
+                    print("Artificer: Better at STEM.")
+                    print("Bard: Better at the Humanities")
+                    char_class = input("What type of student are you? ").capitalize()
+                    if char_class == "Artificer" or "Bard":
+                        file.write(f"Class: {char_class}\n")
+                        break
 
 main()
